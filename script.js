6fd0b61f28049772b549cf374bc981e99ab90240
Tao (Larissa) <25f5k252l@mozmail.com>
2023-01-02 16:16:50 +0300

    Create mobile menu using javascript

---
+++ script.js
@@ -0,0 +1,21 @@
+//query variables
+const body = document.querySelector('body');
+const nav = document.querySelector('nav');
+const popupMenu = document.querySelector('.popup-menu');
+const closeBtn = document.querySelector('.close-btn');
+const menuOption = document.querySelector('.popup-menu ul');
+
+const openMenu = () => {
+  popupMenu.style.display = 'block';
+  body.classList.add('noscroll');
+  popupMenu.style.animation = 'fadein 2s';
+}
+
+const closeMenu = () => {
+  popupMenu.style.display = 'none'; 
+  body.classList.remove('noscroll');
+}
+
+nav.addEventListener('click', openMenu);
+closeBtn.addEventListener('click', closeMenu);
+menuOption.addEventListener('click', closeMenu);
