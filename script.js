8c0ee62f97bcc73f57a4dcffdf8f1d40c8d78e82
Tao (Larissa) <25f5k252l@mozmail.com>
2023-01-03 19:03:55 +0300

    Add the first part of the project popup window

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* mobile menu */
 // query variables
 const body = document.querySelector('body');
 const nav = document.querySelector('nav');
@@ -19,3 +20,69 @@ const closeMenu = () => {
 nav.addEventListener('click', openMenu);
 closeBtn.addEventListener('click', closeMenu);
 menuOption.addEventListener('click', closeMenu);
+
+/* Project details popup window */
+// query variables
+
+const projectDetails = [
+  {
+    name: 'project name one',
+    description: 'project description one',
+    featuredImage: 'img/mobile/one.png',
+    technologies: ['html/css', 'ruby on rails', 'javascript'],
+    live: 'link/to/live',
+    source: 'link/to/source',
+  },
+  {
+    name: 'project name two',
+    description: 'project description two',
+    featuredImage: 'path/to/featured/image',
+    technologies: ['html/css', 'ruby on rails', 'javascript'],
+    live: 'link/to/live',
+    source: 'link/to/source',
+  },
+  {
+    name: 'project name three',
+    description: 'project description three',
+    featuredImage: 'path/to/featured/image',
+    technologies: ['html/css', 'ruby on rails', 'javascript'],
+    live: 'link/to/live',
+    source: 'link/to/source',
+  },
+  {
+    name: 'project name four',
+    description: 'project description four',
+    featuredImage: 'path/to/featured/image',
+    technologies: ['html/css', 'ruby on rails', 'javascript'],
+    live: '#',
+    source: '#',
+  },
+  {
+    name: 'project name five',
+    description: 'project description five',
+    featuredImage: 'path/to/featured/image',
+    technologies: ['html/css', 'ruby on rails', 'javascript'],
+    live: '#',
+    source: '#',
+  },
+  {
+    name: 'project name six',
+    description: 'project description six',
+    featuredImage: 'path/to/featured/image',
+    technologies: ['html/css', 'ruby on rails', 'javascript'],
+    live: '#',
+    source: '#',
+  },
+];
+
+const projectDetailsWindow = document.querySelector('aside');
+const projectName = document.querySelector('.project-details__name');
+
+const firstProject = document.querySelector('.project__item_one .project__visit-btn');
+
+const openProject = () => {
+  projectDetailsWindow.style.display = 'block';
+  projectName.innerText = projectDetails[0].name;
+};
+
+firstProject.addEventListener('click', openProject);
